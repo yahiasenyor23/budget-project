ad4c02651fcfe9bc47fe0dfa5ead022fdf89d3d7
yahiasenyor23 <yahiasaad111468@gmail.com>
2025-03-19 02:09:28 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+r=10
+print("inter rice price",r) #r is the price of rice
+m=70
+print("inter meat price ",m )  #m is the price of meat
+f=30
+print("inter fish price ",f) # f is the price of fish
+total = r+m+f
+print(total)
+budget=170
+t= budget-total
+print(t)
+
